b57f9ccb34f0676f6504023aa31a18277f13b6f0
Kenneth Gibson <scaphoidederps@gmail.com>
2022-07-03 08:03:27 +0300

    Add project files.

---
+++ OnlineStoreWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineStoreWebsite.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
